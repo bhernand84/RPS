44fb79d3532c9871e64efd0e26f49da4bf356d60
Benaldo <bhernandez@tonicdesign.com>
2015-03-05 00:02:36 -0500

    First two tests committed

---
+++ RPS.Models/Move.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPS.Models
+{
+    public enum Move
+    {
+        Rock = 0,
+        Paper,
+        Scissors
+    }
+}
---
+++ Tests/MoveTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using RPS.Models;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MoveTests
+    {
+
+        [Test]
+        public void RockBeatsScissors()
+        {
+            Assert.True(Move.Rock < Move.Paper);
+        }
+        [Test]
+        public void PaperBeatsRock()
+        {
+            Assert.True(Move.Paper > Move.Rock);
+        }
+    }
+}       
